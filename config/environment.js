26795fc5714d2b2bcf86830759a337faa33d8b20
Mollie Bootsma and Michael Gallegos <mklgallegos@gmail.com>
2016-03-30 11:55:07 -0700

    post tile and individual post view functional

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dev-blog',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://dev-blog.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
